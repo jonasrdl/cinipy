029f483690bdfd3a853fcf19b3334ec8cf2fa29a
jonasrdl <jonasriedel@protonmail.com>
2023-05-19 21:40:16 +0200

    fix: bump version to `0.0.2`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cinipy',
-    version='0.0.1',
+    version='0.0.2',
     author='jonasrdl',
     author_email='jonasriedel@pm.me',
     description='An app for custom logging',
